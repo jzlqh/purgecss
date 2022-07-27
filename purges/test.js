387317d7e67871ab9a6f094463a385fbef9cf076
eden.jia <eden.jia@yijinin.com>
2022-07-27 14:43:30 +0800

    add: purges

---
+++ purges/test.js
@@ -0,0 +1,15 @@
+const postcss = require('postcss');
+const purge = require('./src/index');
+const fs = require('fs');
+const path = require('path');
+const css = fs.readFileSync('./example/index.css');
+
+const c = postcss([purge({
+    html: path.resolve('./example/index.html'),
+})])
+
+c.process(css).then(result => {
+    // console.log(result.css);
+});
+
+
